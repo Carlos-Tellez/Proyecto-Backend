8ea0168c82ef0fc4f76a2d84966f90b52e660b57
Carlos Tellez <carlos_tellez@bancopopular.com.co>
2023-08-03 13:33:48 -0500

    Vista con Handlebars

--- src/index.js
+++ src/index.js
@@ -1,16 +1,51 @@
 import express from "express";
-import ProductRouter from "./router/product.routes.js";
-import CartRouter from "./router/carts.routes.js";
+import ProductoR from "./router/product.routes.js"
+import CartR from "./router/cart.routes.js"
+import {engine} from "express-handlebars"
+import * as path from "path"
+import __dirname from "./utils.js";
+import ProductManager from "./controlador/productManager.js";
+import VistaR from "./router/vista.routes.js";
+import { Server } from "socket.io"
+import VistaRealTimeR from "./router/realTimeProducts.routes.js";
 
 const app = express();
-const port = 8080;
+const producto = new ProductManager();
+
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-app.use("/products",ProductRouter)
-app.use("/cart",CartRouter)
+//handlebars
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views", path.resolve(__dirname + "/views"))
 
-app.listen(port, () => {
-    console.log (`Servidor Express Puerto ${port}`);
+//static
+app.use("/", express.static(__dirname + "/public"))
+
+
+
+
+app.use("/productos",ProductoR)
+app.use("/cart",CartR)
+app.use("/view",VistaR)
+app.use("/viewR",VistaRealTimeR)
+
+app.listen(8080, ()=>{
+    const httpServer = app.listen(8080, ()=>{
+    console.log("servidor arriba en el puerto: 8080" );
 });
+});
+
+
+//socketserver
+
+const socketServer = new Server(httpServer)
+
+socketServer.on("connection", socket => {
+
+    console.log("client connected by sockets")
+
+
+})
